167f55f020dee195615272995ed130a0c8b56540
Gabriel ADELAIDE <gabriel.adelaide@bordeaux-inp.fr>
2025-11-21 11:16:37 +0100

    meilleur lisibilité pour la fonction homography_apply

--- homography_estimate.py
+++ homography_estimate.py
@@ -6,15 +6,9 @@ def homography_estimate(x1, y1, x2, y2):
 
 def homography_apply(H, x1, y1):
     # extraire les coefs de H
-    h11 = H[0][0]
-    h12 = H[0][1]
-    h13 = H[0][2]
-    h21 = H[1][0]
-    h22 = H[1][1]
-    h23 = H[1][2]
-    h31 = H[2][0]
-    h32 = H[2][1]
-    h33 = H[2][2]
+    h11, h12, h13 = H[0][0], H[0][1], H[0][2]
+    h21, h22, h23 = H[1][0], H[1][1], H[1][2]
+    h31, h32, h33 = H[2][0], H[2][1], H[2][2]
 
     x2 = (h11 * x1 + h12 * y1 + h13)/(h31 * x1 + h32 * y1 + h33)
     y2 = (h21 * x1 + h22 * y1 + h23)/(h31 * x1 + h32 * y1 + h33)
